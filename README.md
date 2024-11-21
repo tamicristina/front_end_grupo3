a9a99a98ccf62583df58f70769a6a5c2d8ea23b9
tamicristina <tami.cristina06@gmail.com>
2024-11-21 20:28:00 -0300

    second commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Integrantes
+
+- Matheus da Silva Freitas
+- Victor Rodrigues dos Santos
+- Rafael Bezerra Cena
+- Priscila Jenny Nala
+- Tamires Cristina da Silva
+- Allison
--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 // Array com as mensagens
 const mensagens = [
-    {titulo: "Boa noite!!", corpo:"Você é a luz do meu dia!"},
-    {titulo: "OI!!", corpo:"Adoro o seu sorriso!"},
-    {titulo: "Titulo 2", corpo:"Você é incrível!"},
-    {titulo: "Titulo 3", corpo:"Hoje é um dia especial porque você está aqui!"}
-]
+  { titulo: "Boa noite!!", corpo: "Você é a luz do meu dia!" },
+  { titulo: "OI!!", corpo: "Adoro o seu sorriso!" },
+  { titulo: "Titulo 2", corpo: "Você é incrível!" },
+  {
+    titulo: "Titulo 3",
+    corpo: "Hoje é um dia especial porque você está aqui!",
+  },
+];
 
 // Função para trocar as mensagens
-function atualiarMensagem(){
-    const indiceAleatorio = Math.floor(Math.random() * mensagens.length);
-    const mensagem = mensagens[indiceAleatorio];
+function atualiarMensagem() {
+  const indiceAleatorio = Math.floor(Math.random() * mensagens.length);
+  const mensagem = mensagens[indiceAleatorio];
 
-    // Atualiza o título
-    document.getElementById('titulo').innerText = mensagem.titulo;
+  // Atualiza o título
+  document.getElementById("titulo").innerText = mensagem.titulo;
 
-    // Atualiza o corpo
-    document.getElementById('corpo').innerText = mensagem.corpo;
+  // Atualiza o corpo
+  document.getElementById("corpo").innerText = mensagem.corpo;
 }
 
 // Atualizar a mensagem a cada 5 segundos
-setInterval(atualiarMensagem, 5000);
+setInterval(atualiarMensagem, 3000);
 
 // Atualizar imediatamente quando a página for
 atualiarMensagem();
-
-
-
